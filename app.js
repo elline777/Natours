51747762f84a4b2ba641294c5ac511ebd529ce68
Natalia <47737431+elline777@users.noreply.github.com>
2020-03-23 00:03:39 +0500

    Catch errors during development and production

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -24,5 +26,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
+});
+
+// Error handling middlewate
+app.use(globalErrorHandler);
 
 module.exports = app;
