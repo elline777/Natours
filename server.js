f72415a102278bb2ccb15b74117f8ff2cfe4e7a2
Natalia <47737431+elline777@users.noreply.github.com>
2020-03-27 21:03:48 +0500

    Create user model, user controller, encrypt password

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT REJECTION. Shutting down....');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -27,7 +33,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
   console.log(err.name, err.message);
-  console.log('Unhandled rejection. Shutting down....');
+  console.log('UNHANDLED REJECTION. Shutting down....');
   server.close(() => {
     process.exit(1);
   });
